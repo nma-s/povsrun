1b1cb320cd77e2dd6c1b5d595578812823549ebd
nsamuels <n-samuels@outlook.com>
2025-08-18 11:10:16 +0100

    added annotations, fields, getters and setters to Runners class

--- src/main/java/com/cbfacademy/povsrun_group/runners/Runner.java
+++ src/main/java/com/cbfacademy/povsrun_group/runners/Runner.java
@@ -1,7 +1,57 @@
 package com.cbfacademy.povsrun_group.runners;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
-@Table("Runner")
+@Table(name="runner")
 public class Runner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    public UUID id;
+    public String firstName;
+    public String lastName;
+
+    @Enumerated(EnumType.STRING)
+    public Gender gender;
+
+    public Runner(String firstName, String lastName, String gender){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = Gender.convertInput(gender);
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setFirstName(String fName) {
+        this.firstName = fName;
+    }
+
+    public String getLastName(){
+        return lastName; 
+    }
+
+    public void setLastName(String lName) {
+        this.lastName = lName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
 
 }

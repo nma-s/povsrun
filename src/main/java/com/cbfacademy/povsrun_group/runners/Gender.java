03b26fb0d09dc0cbd5180f61b84a5210358bae0f
nsamuels <n-samuels@outlook.com>
2025-08-18 11:04:09 +0100

    updated Gender enum with converInput method

--- src/main/java/com/cbfacademy/povsrun_group/runners/Gender.java
+++ src/main/java/com/cbfacademy/povsrun_group/runners/Gender.java
@@ -1,18 +1,38 @@
 package com.cbfacademy.povsrun_group.runners;
 
 public enum Gender {
-FEMALE("F"),
-MALE("M"),
-OTHER("O")
+    FEMALE("F"),
+    MALE("M"),
+    OTHER("O"),
+    PREFER_NOT_TO_SAY("Prefer Not To Say");
 
-private final String gender;
+    private final String gender;
 
-Gender(String gender) {
-    this.gender = gender;
-}
+    Gender(String gender) {
+        this.gender = gender;
+    }
 
-public String getGender(){
-    return gender;
-}
+    public static Gender convertInput(String value) {
+        if (value == null)
+            return PREFER_NOT_TO_SAY;
+        String v = value.trim().toUpperCase();
+        switch (v) {
+            case "F":
+            case "FEMALE":
+                return FEMALE;
+            case "M":
+            case "MALE":
+                return MALE;
+            case "O":
+            case "OTHER":
+                return OTHER;
+            default:
+                throw new IllegalArgumentException("Invalid Gender Input");
+        }
+    }
+
+    public String getGender() {
+        return gender;
+    }
 
 }

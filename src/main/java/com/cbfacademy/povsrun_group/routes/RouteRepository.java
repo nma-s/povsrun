45fc3ebb121d5a43438d1fb22661cc3efb013c4b
nsamuels <n-samuels@outlook.com>
2025-08-26 11:44:08 +0100

    Created Route Repository interface

---
+++ src/main/java/com/cbfacademy/povsrun_group/routes/RouteRepository.java
@@ -0,0 +1,8 @@
+package com.cbfacademy.povsrun_group.routes;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface RouteRepository extends ListCrudRepository<Route, Long> {
+
+    
+} 

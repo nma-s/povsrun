4b16d3205ef239166de005c59b172e1328692daf
nsamuels <n-samuels@outlook.com>
2025-08-28 16:49:55 +0100

    added derived query findByDistance to RouteRepository

--- src/main/java/com/cbfacademy/povsrun_group/routes/RouteRepository.java
+++ src/main/java/com/cbfacademy/povsrun_group/routes/RouteRepository.java
@@ -1,8 +1,11 @@
 package com.cbfacademy.povsrun_group.routes;
 
+import java.util.List;
+
 import org.springframework.data.repository.ListCrudRepository;
 
 public interface RouteRepository extends ListCrudRepository<Route, Long> {
 
+    public List<Route> findByDistanceInKm(int distance);
     
 } 

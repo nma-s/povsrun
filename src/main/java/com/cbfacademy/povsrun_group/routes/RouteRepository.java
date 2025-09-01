f88f506577c60a5753981b08cd030b6f43b7bd4b
nsamuels <n-samuels@outlook.com>
2025-09-01 15:00:17 +0100

    removed derived query method findByViaRouteContaining and replaced with JPQL query findRoutesByViaRoute

--- src/main/java/com/cbfacademy/povsrun_group/routes/RouteRepository.java
+++ src/main/java/com/cbfacademy/povsrun_group/routes/RouteRepository.java
@@ -2,12 +2,17 @@ package com.cbfacademy.povsrun_group.routes;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RouteRepository extends ListCrudRepository<Route, Long> {
 
     public List<Route> findByDistanceInKm(int distance);
 
-    public List<Route> findByViaRoutesContaining(String viaString);
+    // public List<Route> findByViaRouteContaining(String viaRoute);
+
+    @Query("SELECT r FROM Route r JOIN r.viaRoute v WHERE lower(v) LIKE CONCAT('%',lower(:viaRoute),'%')")
+    public List<Route> findRoutesByViaRoute(@Param("viaRoute") String viaRoute);
     
 } 

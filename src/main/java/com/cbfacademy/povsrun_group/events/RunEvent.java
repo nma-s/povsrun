cb2c8bf4cbd1186a54939dad8bb84dc2f67c1d65
nsamuels <n-samuels@outlook.com>
2025-09-01 17:45:55 +0100

    changed Id type to Long - IDENTITY and added getters and setters

--- src/main/java/com/cbfacademy/povsrun_group/events/RunEvent.java
+++ src/main/java/com/cbfacademy/povsrun_group/events/RunEvent.java
@@ -1,13 +1,16 @@
 package com.cbfacademy.povsrun_group.events;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 import java.util.UUID;
 
+import com.cbfacademy.povsrun_group.routes.Route;
 import com.cbfacademy.povsrun_group.runners.Runner;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -16,12 +19,37 @@ import jakarta.persistence.Table;
 public class RunEvent {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-    public UUID id;
+    public Long id;
     public LocalDate date;
-    public String month;
-    public List<Runner> particpants;
-    public String image;
-    public String route
+    public Month month;
+    // public List<Runner> particpants;
+    // public String image;
+    // public Route route;
+
+    public RunEvent(String date){
+        this.date = LocalDate.parse(date);
+        this.month = LocalDate.parse(date).getMonth();
+    }
+
+    public RunEvent(){
+        this.date = LocalDate.parse("2025-01-01");
+    }
+
+    public LocalDate getDate(){
+        return date;
+    }
+
+    public void getDate(String date){
+        this.date = LocalDate.parse(date);
+    }
+
+    public Month getMonth() {
+        return month;
+    }
+
+
+
+
 }

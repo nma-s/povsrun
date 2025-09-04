709f2bb4fbc123bb1df8581bbe5a82d6a0b89c17
nsamuels <n-samuels@outlook.com>
2025-09-04 14:08:59 +0100

    added  many to many relationship with runEvent

--- src/main/java/com/cbfacademy/povsrun_group/routes/Route.java
+++ src/main/java/com/cbfacademy/povsrun_group/routes/Route.java
@@ -1,9 +1,13 @@
 package com.cbfacademy.povsrun_group.routes;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.cbfacademy.povsrun_group.events.RunEvent;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
@@ -13,7 +17,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,10 +26,11 @@ import jakarta.persistence.Table;
 public class Route {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @SequenceGenerator(name = "route-sequence", initialValue = 100, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "route-sequence")
 
     public Long routeId;
-    public Integer distanceInKm;
+    public Float distanceInKm;
     public String startingPoint;
 
     @ElementCollection
@@ -32,18 +38,19 @@ public class Route {
     
     @Column(name="via_route")
     public List<String> viaRoute;
+    
+    @JsonIgnore
+    @ManyToMany(mappedBy = "assignedRoutes" )
+    protected Set<RunEvent> runEventSet = new HashSet<>();
 
-    @ManyToOne
-    public RunEvent runEvent;
-
-    public Route(Integer distance, String startingPoint, List<String> via){
+    public Route(Float distance, String startingPoint, List<String> via){
         this.distanceInKm = distance;
         this.startingPoint = startingPoint;
         this.viaRoute = new ArrayList<>(via);
     }
     
     public Route(){
-        this.distanceInKm = 5;
+        this.distanceInKm = 5f;
         this.startingPoint = "Elephant and Castle Square";
         this.viaRoute = new ArrayList<>(List.of("London Brigde", "Waterloo Bridge"));
     }
@@ -52,11 +59,11 @@ public class Route {
         return routeId;
     }
 
-    public Integer getDistanceInKm(){
+    public Float getDistanceInKm(){
         return distanceInKm; 
     }
 
-    public void setDistanceInKm(Integer distance){
+    public void setDistanceInKm(Float distance){
         this.distanceInKm = distance;
     }
 
@@ -75,5 +82,4 @@ public class Route {
     public void setViaRoute(List<String> via) {
         this.viaRoute = via;
     }
-
 }
